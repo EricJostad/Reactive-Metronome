e42a537dc6b9e9595deac58cd04004c70decaebf
EJostad <eric.jostad@outlook.com>
2021-03-26 23:39:48 -0700

    Added slider bar for BPM to Metronome.js.

--- reactive-metronome/src/Metronome.js
+++ reactive-metronome/src/Metronome.js
@@ -3,7 +3,18 @@ import './Metronome.css';
 
 class Metronome extends Component {
     render() {
-        return <div className='metronome'>hi</div>;
+        const bpm = 100;
+        const playing = false;
+
+        return (
+            <div className='metronome'>
+                <div className='bpm=slider'>
+                    <div>{bpm} BPM</div>
+                    <input type='range' min='10' max='300' value={bpm} />
+                </div>
+                <button>{playing ? 'Stop' : 'Start'}</button>
+            </div>
+        )
     }
 }
 
